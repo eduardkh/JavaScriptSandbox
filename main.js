d22c27d18b17548d9b1c2518eff2eb7cf7fd310e
Eduard Khiaev <eddietheone1984@gmail.com>
2018-12-10 22:47:33 +0200

    For and ForEach Loops

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 document.getElementById("container").innerHTML = "JavaScript sandbox Test DIV";
 console.log("JavaScript sandbox Test Console");
+
+const colors = ["red", "green", "blue"];
+
+// Reguler old For Loop
+for (let i = 0; i < colors.length; i++) {
+  const colorsElement = colors[i];
+  console.log(colorsElement);
+}
+
+// ForEach Loop
+colors.forEach(function(color) {
+  console.log(color);
+});
+// ForEach Loop with a Fat Arrow
+colors.forEach(color => {
+  console.log(color);
+});
+
+// ForEach Loop - adding numbers
+const numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+
+numbers.forEach(function(number) {
+  sum += number;
+});
+console.log(sum);
+
+// ForEach Loop - adding numbers (Reference to External Function)
+function adder(number) {
+  sum += number;
+}
+numbers.forEach(adder);
+console.log(sum);
