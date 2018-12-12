e6dce26b82745e7af0b22fcf7b5b3f77f06a8a4c
Eduard Khiaev <eddietheone1984@gmail.com>
2018-12-12 14:23:14 +0200

    Map Helper

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.getElementById("container").innerHTML = "JavaScript sandbox Test DIV";
 console.log("JavaScript sandbox Test Console");
 
+/*
+//  ***ForEach Loop***
 const colors = ["red", "green", "blue"];
 
 // Reguler old For Loop
@@ -33,3 +35,29 @@ function adder(number) {
 }
 numbers.forEach(adder);
 console.log(sum);
+*/
+
+// ***Map Loop***
+const numbers = [1, 2, 3];
+let doubleNumbers = [];
+//the way it was done in the past
+for (let i = 0; i < numbers.length; i++) {
+  doubleNumbers.push(numbers[i] * 2);
+}
+console.log(doubleNumbers);
+//Map Loop
+const doubled = numbers.map(function(number) {
+  return number * 2;
+});
+console.log(doubled);
+
+const cars = [
+  { model: "buick", price: "Cheap" },
+  { model: "Camero", price: "Expensive" }
+];
+
+const prices = cars.map(function(car) {
+  return car.price;
+});
+
+console.log(prices);
