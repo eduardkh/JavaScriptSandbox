df5baa9fdb416cd07a249d6775a9db46b9238378
Eduard Khiaev <eddietheone1984@gmail.com>
2018-12-12 15:09:28 +0200

    Filter Helper

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function adder(number) {
 }
 numbers.forEach(adder);
 console.log(sum);
-*/
+
 
 // ***Map Loop***
 const numbers = [1, 2, 3];
@@ -61,3 +61,54 @@ const prices = cars.map(function(car) {
 });
 
 console.log(prices);
+
+*/
+// ***Filter Loop***
+const products = [
+  { name: "cucumber", type: "vegetable" },
+  { name: "banana", type: "fruite" },
+  { name: "celery", type: "vegetable" },
+  { name: "orange", type: "fruite" }
+];
+// Reguler old For Loop
+const filterdProducts = [];
+for (let i = 0; i < products.length; i++) {
+  if (products[i].type === "fruite") {
+    filterdProducts.push(products[i]);
+  }
+}
+console.log(filterdProducts);
+
+const newfilterdProducts = products.filter(function(product) {
+  return product.type === "fruite";
+});
+console.log(newfilterdProducts);
+
+const complexproducts = [
+  { name: "cucumber", type: "vegetable", quantity: 0, price: 1 },
+  { name: "banana", type: "fruite", quantity: 10, price: 15 },
+  { name: "celery", type: "vegetable", quantity: 30, price: 9 },
+  { name: "orange", type: "fruite", quantity: 3, price: 5 }
+];
+//type is 'vegetable', quantity is greater than 0, price is less than 10
+const complexfilterdProducts = complexproducts.filter(function(product) {
+  return (
+    product.type === "vegetable" && product.quantity > 0 && product.price < 10
+  );
+});
+console.log(complexfilterdProducts);
+
+const post = { id: 4, title: "New Post" };
+const comments = [
+  { postId: 4, content: "awesome post" },
+  { postId: 3, content: "it was OK" },
+  { postId: 4, content: "neat" }
+];
+
+function commentsForPost(post, comments) {
+  return comments.filter(function(comment) {
+    return comment.postId === post.id;
+  });
+}
+
+console.log(commentsForPost(post, comments));
